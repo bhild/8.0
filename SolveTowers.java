2582b378c62c04596d332db1a59840fa282ddd22
bhild <42156370+bhild@users.noreply.github.com>
2020-02-18 13:18:22 -0800

    Add files via upload

---
+++ SolveTowers.java
@@ -0,0 +1,21 @@
+package chapter8;
+
+//********************************************************************
+// SolveTowers.java Author: Lewis/Loftus
+//
+// Demonstrates recursion.
+//
+//********************************************************************
+public class SolveTowers
+{
+
+//-----------------------------------------------------------------
+ // Creates a TowersOfHanoi puzzle and solves it.
+
+//-----------------------------------------------------------------
+	public static void main (String[] args)
+	{
+		TowersOfHanoi towers = new TowersOfHanoi(4);
+		towers.solve();
+ 	}
+}
